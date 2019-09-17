9ddba202368eca4ffb4d472d8eaad061559842d1
Ameer Hamza Naveed <ameerhmzx@gmail.com>
2019-09-17 12:37:51 +0500

    Update Main.java

--- lab4/Home Task/Main.java
+++ lab4/Home Task/Main.java
@@ -2,6 +2,7 @@ class Main {
 
   public static void main(String[] args) {
 
+  
     // Create cylinder object from class Cylinder
     Cylinder cylinder = new Cylinder();
 
@@ -11,10 +12,20 @@ class Main {
 
     // Getting Values
     System.out.println(cylinder.getHeight());
-    System.out.println(cylinder.getHeight());
+    System.out.println(cylinder.getRadius());
 
     //print Details
     cylinder.displayInfo();
+
+
+    //calling each constructor/method at least once
+    Cylinder cylinder1 = new Cylinder(2f);
+    System.out.println(cylinder1.getHeight());
+    System.out.println(cylinder1.getRadius());
+
+    Cylinder cylinder2 = new Cylinder(2f, 3f);
+    System.out.println(cylinder2.getHeight());
+    System.out.println(cylinder2.getRadius());
 
   }
 }
