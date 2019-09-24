300aa1d359579e4a9258da10d04bb582b776f6d5
Ameer Hamza Naveed <ameerhmzx@gmail.com>
2019-09-24 12:32:42 +0500

    Create TestCalculator.java

---
+++ lab5/TestCalculator.java
@@ -0,0 +1,21 @@
+package lab.task5;
+
+
+import java.util.Scanner;
+
+public class TestCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter Number 1: ");
+        double op1 = scanner.nextDouble();
+        
+        System.out.println("Enter Number 2: ");
+        double op2 = scanner.nextDouble();
+        
+        System.out.println("Enter Choice: \n\'+\' to add\n\'-\' to subtract\n\'*\' to Multiply\n\'/\' to divide\n\'^\' for Power\n\'&\' for Square Root\n> ");
+        char ch = scanner.next().charAt(0);
+        
+        Calculator calculator = new Calculator(op1, op2, ch);
+    }
+}
