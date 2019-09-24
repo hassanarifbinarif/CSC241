c08a6126947a2d8b07e1f462ab6c523f9488b8d2
Ameer Hamza Naveed <ameerhmzx@gmail.com>
2019-09-24 12:33:36 +0500

    Update Calculator.java

--- lab5/Calculator.java
+++ lab5/Calculator.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab.task5;
 
-/**
- *
- * @author admin
- */
+
 public class Calculator {
     private double operand1;
     private double operand2;
