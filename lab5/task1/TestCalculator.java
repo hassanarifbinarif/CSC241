72647f8c7e3d7f5346e5f071031496b828f98f7e
Ameer Hamza Naveed <ameerhmzx@gmail.com>
2019-09-24 12:48:20 +0500

    Update TestCalculator.java

--- lab5/task1/TestCalculator.java
+++ lab5/task1/TestCalculator.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 
 public class TestCalculator {
     public static void main(String[] args) {
+        
+        Calculator calculator1;
+        Calculator calculator2 = new Calculator();
+        
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Enter Number 1: ");
+        System.out.print("Enter Number 1: ");
         double op1 = scanner.nextDouble();
         
-        System.out.println("Enter Number 2: ");
+        System.out.print("Enter Number 2: ");
         double op2 = scanner.nextDouble();
         
-        System.out.println("Enter Choice: \n\'+\' to add\n\'-\' to subtract\n\'*\' to Multiply\n\'/\' to divide\n\'^\' for Power\n\'&\' for Square Root\n> ");
+        System.out.print("Enter Choice: \n\'+\' to add\n\'-\' to subtract\n\'*\' to Multiply\n\'/\' to divide\n\'^\' for Power\n\'&\' for Square Root\n> ");
         char ch = scanner.next().charAt(0);
         
-        Calculator calculator = new Calculator(op1, op2, ch);
+        calculator1 = new Calculator(op1, op2, ch);
     }
 }
