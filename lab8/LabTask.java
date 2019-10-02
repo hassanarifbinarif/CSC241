f994502f3edf2a559ab91bbb232c5506a6bfdbce
Nauman Umer <nmanumr@gmail.com>
2019-10-02 10:44:41 +0500

    Update LabTask.java

--- lab8/LabTask.java
+++ lab8/LabTask.java
@@ -1,30 +1,27 @@
-
-public class LabTask {
-
-    public static void main(String[] args) {
-        System.out.println(RandomCharacter.getRandomLowerCase());
-    }
+class Main {
+  public static void main(String[] args) {
+    System.out.println(RandomCharacter.getRandomLowerCase());
+  }
 }
 
 class RandomCharacter {
+  public static char getRandomCharacter() {
+    return (char) (Math.random() * 65536);
+  }
 
-    public static char getRandomCharacter() {
-        return (char) (Math.random() * 65536);
-    }
+  public static char getRandomCharacter(char min, char max) {
+    return (char) (Math.random() * (max - min + 1) + min);
+  }
 
-    public static char getRandomCharacter(char ch1, char ch2) {
-        return (char)(Math.random() * (max-min +1) +min);
-    }
+  public static char getRandomLowerCase() {
+    return getRandomCharacter('a', 'z');
+  }
 
-    public static char getRandomLowerCase() {
-        return getRandomCharacter('a', 'z');
-    }
+  public static char getRandomUpperCase() {
+    return getRandomCharacter('A', 'Z');
+  }
 
-    public static char getRandomUpperCase() {
-        return getRandomCharacter('A', 'Z');
-    }
-
-    public static char getRandomDigit() {
-        return getRandomCharacter('0', '9');
-    }
+  public static char getRandomDigit() {
+    return getRandomCharacter('0', '9');
+  }
 }
