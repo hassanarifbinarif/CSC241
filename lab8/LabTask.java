62c15bb28966ef837fea33dbdd0a9cd2ee02e0f3
Ameer Hamza Naveed <ameerhmzx@gmail.com>
2019-10-02 10:39:12 +0500

    Update and rename LabTask2.java to LabTask.java

---
+++ lab8/LabTask.java
@@ -0,0 +1,33 @@
+
+public class LabTask {
+
+    public static void main(String[] args) {
+        System.out.println(RandomCharacter.getRandomLowerCase());
+    }
+}
+
+class RandomCharacter {
+
+    public static char getRandomCharacter() {
+        return (char) (Math.random() * 65536);
+    }
+
+    public static char getRandomCharacter(char ch1, char ch2) {
+        return Math.random() < .5 ? ch1 : ch2;
+    }
+
+    public static char getRandomLowerCase() {
+        int max = 122, min = 97;
+        return (char) (Math.random() * (max - min + 1) + min);
+    }
+
+    public static char getRandomUpperCase() {
+        int max = 67, min = 90;
+        return (char) (Math.random() * (max - min + 1) + min);
+    }
+
+    public static char getRandomDigit() {
+        int max = 48, min = 57;
+        return (char) (Math.random() * (max - min + 1) + min);
+    }
+}
