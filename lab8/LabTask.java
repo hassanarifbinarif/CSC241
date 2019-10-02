1a5269bc70c8b80772460b0c10ed533c83153ed6
Ameer Hamza Naveed <ameerhmzx@gmail.com>
2019-10-02 11:12:11 +0500

    Update LabTask.java

--- lab8/LabTask.java
+++ lab8/LabTask.java
@@ -1,27 +1,70 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println(RandomCharacter.getRandomLowerCase());
-  }
+package javaapplication1;
+
+
+public class LabTask {
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+class CountLettersInArray{
+
+    private char[] arr;
+    private int[] count;
+   
+    char[] createArray(){
+        arr = new char[100];
+        return arr;
+    }
+    
+    void displayCharacterArray(){
+        for(char a : arr){
+            System.out.print(a+", ");
+        }
+    }
+    
+    void displayCountsArray(){
+        for(int a : count){
+            System.out.print(a+", ");
+        }
+    }
+    
+    int[] countCharacters(char[] ch){
+        count = new int[26];
+        for(int i = 0; i<26; i++){
+            for(char a : ch){
+               if(ch[i] == a)
+                   count[i]++;
+            }
+        }
+        return count;
+    }
+    
 }
 
 class RandomCharacter {
-  public static char getRandomCharacter() {
-    return (char) (Math.random() * 65536);
-  }
 
-  public static char getRandomCharacter(char min, char max) {
-    return (char) (Math.random() * (max - min + 1) + min);
-  }
+    public static char getRandomCharacter() {
+        return (char) (Math.random() * 65536);
+    }
 
-  public static char getRandomLowerCase() {
-    return getRandomCharacter('a', 'z');
-  }
+    public static char getRandomCharacter(char ch1, char ch2) {
+        return Math.random() < .5 ? ch1 : ch2;
+    }
 
-  public static char getRandomUpperCase() {
-    return getRandomCharacter('A', 'Z');
-  }
+    public static char getRandomLowerCase() {
+        int max = 122, min = 97;
+        return (char) (Math.random() * (max - min + 1) + min);
+    }
 
-  public static char getRandomDigit() {
-    return getRandomCharacter('0', '9');
-  }
+    public static char getRandomUpperCase() {
+        int max = 67, min = 90;
+        return (char) (Math.random() * (max - min + 1) + min);
+    }
+
+    public static char getRandomDigit() {
+        int max = 48, min = 57;
+        return (char) (Math.random() * (max - min + 1) + min);
+    }
 }
