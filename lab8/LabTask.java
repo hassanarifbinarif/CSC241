b27c87572590c0d102fa8ad4dc791464f7c1306c
Ameer Hamza Naveed <ameerhmzx@gmail.com>
2019-10-02 11:14:51 +0500

    Update LabTask.java

--- lab8/LabTask.java
+++ lab8/LabTask.java
@@ -1,11 +1,29 @@
-package javaapplication1;
+class LabTask {
+  public static void main(String[] args) {
+    System.out.println(RandomCharacter.getRandomLowerCase());
+  }
+}
 
+class RandomCharacter {
+  public static char getRandomCharacter() {
+    return (char) (Math.random() * 65536);
+  }
 
-public class LabTask {
+  public static char getRandomCharacter(char min, char max) {
+    return (char) (Math.random() * (max - min + 1) + min);
+  }
 
-    public static void main(String[] args) {
-        
-    }
+  public static char getRandomLowerCase() {
+    return getRandomCharacter('a', 'z');
+  }
+
+  public static char getRandomUpperCase() {
+    return getRandomCharacter('A', 'Z');
+  }
+
+  public static char getRandomDigit() {
+    return getRandomCharacter('0', '9');
+  }
 }
 
 class CountLettersInArray{
@@ -41,30 +59,4 @@ class CountLettersInArray{
         return count;
     }
     
-}
-
-class RandomCharacter {
-
-    public static char getRandomCharacter() {
-        return (char) (Math.random() * 65536);
-    }
-
-    public static char getRandomCharacter(char ch1, char ch2) {
-        return Math.random() < .5 ? ch1 : ch2;
-    }
-
-    public static char getRandomLowerCase() {
-        int max = 122, min = 97;
-        return (char) (Math.random() * (max - min + 1) + min);
-    }
-
-    public static char getRandomUpperCase() {
-        int max = 67, min = 90;
-        return (char) (Math.random() * (max - min + 1) + min);
-    }
-
-    public static char getRandomDigit() {
-        int max = 48, min = 57;
-        return (char) (Math.random() * (max - min + 1) + min);
-    }
 }
